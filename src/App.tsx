0293b5cfc44565ce51a304395518c2871d7f4c9d
AlekseySpirin <spirin.mezhmun@yandex.ru>
2024-08-06 18:07:11 +0300

    1.7 Organization of styles. Adding a theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import AboutPage from "./pages/AboutPage/AboutPage";
 import MainPage from "./pages/MainPage/MainPage";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPAge.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
+
 
 const App = () => {
+    const {theme, toggleTheme } = useTheme()
+
+
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная страница</Link>
             <Link to={'/about'}>О сайте</Link>
 
