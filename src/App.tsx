c17382ea6bb65b872a998d8775f5093c7f72160f
Aleksey Spirin <spirin.mezhmun@yandex.ru>
2024-08-07 17:25:40 +0300

    1.8 add helper => classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
-import {Counter} from "./components/Counter";
+
 import './styles/index.scss'
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPAge.async";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 
@@ -17,7 +15,7 @@ const App = () => {
 
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная страница</Link>
             <Link to={'/about'}>О сайте</Link>
