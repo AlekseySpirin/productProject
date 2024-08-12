66fb64ea4904052b30720b4db1292d7ae9b81888
Aleksey Spirin <spirin.mezhmun@yandex.ru>
2024-08-12 17:02:27 +0300

    2.4 Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -14,7 +14,11 @@ const AppRouter = () => {
                     {Object.values(routeConfig).map(({element, path}) => (
                         <Route key={path}
                                path={path}
-                               element={element}
+                               element={(
+                                   <div className={'page-wrapper'}>
+                                       {element}
+                                   </div>
+                               )}
                         />
                     ))}
                 </Routes>
